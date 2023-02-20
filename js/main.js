fa264609c583b083c5d5ce4072fc8316182cb533
RamiroHagen <ramihagen99@gmail.com>
2023-02-20 19:03:33 -0300

    Correcciones

---
+++ js/main.js
@@ -0,0 +1,200 @@
+class Elementos {
+    constructor (nombre, id, img){
+    this.nombre = nombre;
+    this.id = id;
+    this.img = img;
+    this.repeticiones = 0;
+    }
+}
+
+const elemento1 = new Elementos("Bostezar", 1, "../Multimedia/Objetos/Acciones/bostezar.webp");
+const elemento2 = new Elementos("Cantar", 2, "../Multimedia/Objetos/Acciones/Cantar.webp");
+const elemento3 = new Elementos("Chatear", 3, "../Multimedia/Objetos/Acciones/Chatear.webp");
+const elemento4 = new Elementos("Cocer", 4, "../Multimedia/Objetos/Acciones/cocer.webp");
+const elemento5 = new Elementos("Cocinar", 5, "../Multimedia/Objetos/Acciones/cocinar.webp");
+const elemento6 = new Elementos("Comer", 6, "../Multimedia/Objetos/Acciones/comer.webp");
+const elemento7 = new Elementos("Comprar", 7, "../Multimedia/Objetos/Acciones/comprar.webp");
+const elemento8 = new Elementos("Conocer", 8, "../Multimedia/Objetos/Acciones/conocer.webp");
+const elemento9 = new Elementos("Correr", 9, "../Multimedia/Objetos/Acciones/correr.webp");
+const elemento10 = new Elementos("Cortarse el Pelo", 10, "../Multimedia/Objetos/Acciones/cortarseElPelo.webp");
+const elemento11 = new Elementos("Decorar", 11, "../Multimedia/Objetos/Acciones/decorar.webp");
+const elemento12 = new Elementos("Depilarse", 12, "../Multimedia/Objetos/Acciones/depilarse.webp");
+const elemento13 = new Elementos("Despertar", 13, "../Multimedia/Objetos/Acciones/despertar.webp");
+const elemento14 = new Elementos("Dibujar", 14, "../Multimedia/Objetos/Acciones/dibujar.webp");
+const elemento15 = new Elementos("Crear", 15, "../Multimedia/Objetos/Acciones/diseñar.webp");
+const elemento16 = new Elementos("Dormir", 16, "../Multimedia/Objetos/Acciones/dormir.webp");
+const elemento17 = new Elementos("Ejercitarse", 17, "../Multimedia/Objetos/Acciones/ejercitar.webp");
+const elemento18 = new Elementos("Embriagarse", 18, "../Multimedia/Objetos/Acciones/embriagarse.webp");
+const elemento19 = new Elementos("Enamorarse", 19, "../Multimedia/Objetos/Acciones/enamorarse.webp");
+const elemento20 = new Elementos("Enrriquecerse", 20, "../Multimedia/Objetos/Acciones/enrriquecer.webp");
+const elemento21 = new Elementos("Escuchar Musica", 21, "../Multimedia/Objetos/Acciones/escucharMusica.webp");
+const elemento22 = new Elementos("Establecerse", 22, "../Multimedia/Objetos/Acciones/establecerse.webp");
+const elemento23 = new Elementos("Estudiar", 23, "../Multimedia/Objetos/Acciones/estudiar.webp");
+const elemento24 = new Elementos("Fumar", 24, "../Multimedia/Objetos/Acciones/fumar.webp");
+const elemento25 = new Elementos("Charlar", 25, "../Multimedia/Objetos/Acciones/charlar.webp");
+const elemento26 = new Elementos("Hacer las Compras", 26, "../Multimedia/Objetos/Acciones/hacerLasCompras.webp");
+const elemento27 = new Elementos("Hidratarse", 27, "../Multimedia/Objetos/Acciones/hidratarse.webp");
+const elemento28 = new Elementos("Hacer tus Necesidades", 28, "../Multimedia/Objetos/Acciones/irAlBano.webp");
+const elemento29 = new Elementos("Ir al Cine", 29, "../Multimedia/Objetos/Acciones/irAlCine.webp");
+const elemento30 = new Elementos("Ir al Teatro", 30, "../Multimedia/Objetos/Acciones/irAlTeatro.webp");
+const elemento31 = new Elementos("Jugar Juegos de Mesa", 31, "../Multimedia/Objetos/Acciones/JuegosDeMesa.webp");
+const elemento32 = new Elementos("Jugar Videojuegos", 32, "../Multimedia/Objetos/Acciones/jugarVj.webp");
+const elemento33 = new Elementos("Lavar tu Auto", 33, "../Multimedia/Objetos/Acciones/lavarAuto.webp");
+const elemento34 = new Elementos("Lavar los Platos", 34, "../Multimedia/Objetos/Acciones/lavarPlatos.webp");
+const elemento35 = new Elementos("Lavar la Ropa", 35, "../Multimedia/Objetos/Acciones/lavarRopa.webp");
+const elemento36 = new Elementos("Leer", 36, "../Multimedia/Objetos/Acciones/leer.webp");
+const elemento37 = new Elementos("Limpiar", 37, "../Multimedia/Objetos/Acciones/limpiar.webp");
+const elemento38 = new Elementos("Hablar por Telefono", 38, "../Multimedia/Objetos/Acciones/hablarPorTelefono.webp");
+const elemento39 = new Elementos("Llorar", 39, "../Multimedia/Objetos/Acciones/llorar.webp");
+const elemento40 = new Elementos("Manejar", 40, "../Multimedia/Objetos/Acciones/manejar.webp");
+const elemento41 = new Elementos("Matear", 41, "../Multimedia/Objetos/Acciones/matear.webp");
+const elemento42 = new Elementos("Mejorar", 42, "../Multimedia/Objetos/Acciones/mejorar.webp");
+const elemento43 = new Elementos("Meterse al Agua", 43, "../Multimedia/Objetos/Acciones/meterseAlAgua.webp");
+const elemento44 = new Elementos("Mirar Series / Tv", 44, "../Multimedia/Objetos/Acciones/mirarTv.webp");
+const elemento45 = new Elementos("Nadar", 45, "../Multimedia/Objetos/Acciones/nadar.webp");
+const elemento46 = new Elementos("Ordenar", 46, "../Multimedia/Objetos/Acciones/ordenar.webp");
+const elemento47 = new Elementos("Peinarse", 47, "../Multimedia/Objetos/Acciones/peinarse.webp");
+const elemento48 = new Elementos("Pensar", 48, "../Multimedia/Objetos/Acciones/pensar.webp");
+const elemento49 = new Elementos("Pescar", 49, "../Multimedia/Objetos/Acciones/pescar.webp");
+const elemento50 = new Elementos("Planchar", 50, "../Multimedia/Objetos/Acciones/planchar.webp");
+const elemento51 = new Elementos("Plantar", 51, "../Multimedia/Objetos/Acciones/plantar.webp");
+const elemento52 = new Elementos("Rascarse", 52, "../Multimedia/Objetos/Acciones/rascarse.webp");
+const elemento53 = new Elementos("Recibirse", 53, "../Multimedia/Objetos/Acciones/recibirse.webp");
+const elemento54 = new Elementos("Reir", 54, "../Multimedia/Objetos/Acciones/reir.webp");
+const elemento55 = new Elementos("Rezar", 55, "../Multimedia/Objetos/Acciones/rezar.webp");
+const elemento56 = new Elementos("Soñar", 56, "../Multimedia/Objetos/Acciones/sonar.webp");
+const elemento57 = new Elementos("Tener Hijos", 57, "../Multimedia/Objetos/Acciones/tenerHijos.webp");
+const elemento58 = new Elementos("Tocar un Instrumento", 58, "../Multimedia/Objetos/Acciones/tocarInstrumentos.webp");
+const elemento59 = new Elementos("Tomar Alcohol", 59, "../Multimedia/Objetos/Acciones/tomarAlcohol.webp");
+const elemento60 = new Elementos("Tomar Sol", 60, "../Multimedia/Objetos/Acciones/tomarSol.webp");
+const elemento61 = new Elementos("Viajar", 61, "../Multimedia/Objetos/Acciones/viajar.webp");
+
+
+
+const arrayAcciones = [elemento1, elemento2, elemento3, elemento4, elemento5, elemento6, elemento7, elemento8, elemento9, elemento10, elemento11, elemento12, elemento13, elemento14, elemento15, elemento16, elemento17, elemento18, elemento19, elemento20, elemento21, elemento22, elemento23, elemento24, elemento25, elemento26, elemento27, elemento28, elemento29, elemento30, elemento31, elemento32, elemento33, elemento34, elemento35, elemento36, elemento37, elemento38, elemento39, elemento40, elemento41, elemento42, elemento43, elemento44, elemento45, elemento46, elemento47, elemento48, elemento49, elemento50, elemento51, elemento52, elemento53, elemento54, elemento55, elemento56, elemento57, elemento58, elemento59, elemento60, elemento61];
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let iterar = randomArray(arrayAcciones);
+let iterar2 = randomArray(arrayAcciones);
+let cuenta = 0;
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const contenedorJuego = document.getElementById("contenedorJuego");
+const contador = document.getElementById("contador");
+const boton = document.getElementById("candidatos");
+const contenedorCandidatos = document.getElementById("contenedorCandidatos");
+const listaCandidatos = [];
+
+
+
+////////////////////////////////////////MOSTRANDO CARDS/////////////////////////////////////////////////////////
+
+
+
+const mostrarElementos = (Elementos) => {
+    card1.classList.add();
+    card1.innerHTML = `
+                        <div>
+                            <button class= "cardElementos" id= "boton${Elementos.id}">
+                                <img src= "${Elementos.img}" class="imgElementos" alt="${Elementos.nombre}">
+                                <h3 class= "textoElementos">${(Elementos.nombre.toUpperCase())}</h3>
+                            </button>
+                        </div>
+                    `
+
+    contenedorJuego.appendChild(card1);
+
+    const boton = document.getElementById(`boton${Elementos.id}`);
+    boton.addEventListener("click", () => {
+        contadorRondas();
+        accionAumentarRepeticion(Elementos.id);
+        cambiarElemento(Elementos.id);
+    })
+}
+
+
+
+const mostrarElementos2 = (Elementos) => {
+    card2.classList.add();
+    card2.innerHTML = `
+                        <div>
+                            <button class= "cardElementos" id= "boton${Elementos.id}">
+                                <img src= "${Elementos.img}" class="imgElementos" alt="${Elementos.nombre}">
+                                <h3 class= "textoElementos">${(Elementos.nombre.toUpperCase())}</h3>
+                            </button>
+                        </div>
+                    `
+
+    contenedorJuego.appendChild(card2);
+
+    const boton = document.getElementById(`boton${Elementos.id}`);
+    boton.addEventListener("click", () => {
+        contadorRondas();
+        accionAumentarRepeticion(Elementos.id);
+        cambiarElemento(Elementos.id);   
+    })
+}
+
+mostrarElementos2(iterar2);
+mostrarElementos(iterar);
+
+
+
+///////////////////////////////////////FUNCIONES/////////////////////////////////////////////////////////
+
+
+const accionAumentarRepeticion = (id) => {
+    const elementoDefinido = arrayAcciones.find((Elementos) => Elementos.id === id);
+    elementoDefinido.repeticiones++;
+        botonCandidato(elementoDefinido.repeticiones);
+}
+
+ const  cambiarElemento = (id) => { 
+    const cambiarElElemento = arrayAcciones.find((Elementos) => Elementos.id === id);
+    console.log(cambiarElElemento);     
+    let cambiar = randomArray(arrayAcciones);
+    if (mostrarElementos.id == cambiarElElemento.id) {
+        mostrarElementos2(cambiar);  
+    }
+    else if (mostrarElementos2.id == cambiarElElemento.id) {
+        mostrarElementos(cambiar)
+    }
+    else if (mostrarElementos.id !== cambiarElElemento.id) {
+        mostrarElementos(cambiar)
+    }
+    else if (mostrarElementos2.id !== cambiarElElemento.id) {
+        mostrarElementos2(cambiar)
+    }
+}
+
+const botonCandidato = (repeticiones) => {
+    const accionBotonCandidato = arrayAcciones.find((Elementos) => Elementos.repeticiones == repeticiones);
+
+    if (accionBotonCandidato.repeticiones >= 3){
+        boton.innerHTML = `<p>Elegir CANDIDATO</p>
+                            `
+        contenedorJuego.appendChild(boton);
+    }
+}
+
+//CREAR PARA CADA ARRAY//
+function randomArray(arrayAcciones){
+    let random = Math.random()* arrayAcciones.length | 0;
+    let valorRandom = arrayAcciones[random];
+    return valorRandom;
+}
+
+
+function contadorRondas(){
+    cuenta++;
+    contador.innerHTML = `
+                        <div>
+                            <h3 class= "" >RONDA ${cuenta}</h3>
+                        </div>
+                        `
+
+     contenedorJuego.appendChild(contador);
+}
